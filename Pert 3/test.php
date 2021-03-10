c8b7ab80bac395fcfad592ed42b444ced6ed31f8
masy-web <72302847+masy-web@users.noreply.github.com>
2021-03-10 19:56:44 +0700

    Add files via upload

---
+++ Pert 3/test.php
@@ -0,0 +1,45 @@
+<?php
+    
+    $str_1 = "Pemro";
+    $str_2 = "Web";
+
+    echo $str_1.$str_2;
+    for ($i = 0; $i > count()
+    $array = ['Pemro','web','Kelas B']
+    echo $array;
+
+    for ($i = 0; $i > count($array); $i++){
+        echo $array . " ";
+    }
+    
+    foreach($array as $value){
+        echo $value." ";
+    }
+?>
+<?php
+    $contoh_str = "daaw";
+    $contoh_int = 123;
+    $contoh_doub = 2.1;
+    $contoh_arr = [];
+?>
+<?php
+    $mhs = ['nama' => 'Masyhuri',
+    'nim' => '19-1098','usia' => '21'];
+        <tr>
+            <td><?= $mhw['nama'];?></td>
+            <td><?= $mhw['nim'];?></td>
+            <td><?= $mhw['usia'];?></td>
+        </tr>
+    $mhs = [['nama' => 'Masyhuri',
+    'nim' => '19-1098','usia' => '21'],
+    ['nama' => 'Dia',
+    'nim' => '19-1167','usia' => '21']];
+
+    <? foreach($mhs as $mhw); ?>
+        <tr>
+            <td><? echo $mhw['nama']?></td>
+            <td><? echo $mhw['nim']?></td>
+            <td><? echo $mhw['usia']?></td>
+        </tr>
+    <? endforeach; ?>
+?>
